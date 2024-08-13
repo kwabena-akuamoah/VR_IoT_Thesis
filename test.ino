643442ea6e2a13366cf4eb355bc7babaca47bd47
kwabena-akuamoah <123152880+kwabena-akuamoah@users.noreply.github.com>
2024-08-13 01:06:12 +0000

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,15 @@
+
+const int PIN=26;
+void setup() {
+  pinMode(PIN,OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(PIN,HIGH); 
+  // delay(2000);
+  // digitalWrite(PIN,LOW);
+  // delay(2000);
+}
+
+
